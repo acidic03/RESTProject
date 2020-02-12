1a7126d9de357188a69b1e06de62da4c6c16d6e5
Matthew Cohen <mcohen0365@gmail.com>
2020-02-12 00:01:07 -0500

    removed error message field

--- src/main/java/com/matthew/restproject/routes/Statistics.java
+++ src/main/java/com/matthew/restproject/routes/Statistics.java
@@ -3,15 +3,9 @@ package com.matthew.restproject.routes;
 public class Statistics {
 
     public double value;
-    public String error;
 
     public Statistics(double value) {
         this.value = (value);
-        this.error = "";
     }
 
-    public Statistics(double value, String error) {
-        this.value = (value);
-        this.error = error;
-    }
 }
--- src/test/java/com/matthew/restproject/StatisticsTester.java
+++ src/test/java/com/matthew/restproject/StatisticsTester.java
@@ -1,4 +1,14 @@
 package com.matthew.restproject;
 
+import com.matthew.restproject.routes.StatisticsController;
+import org.junit.jupiter.api.Test;
+
 public class StatisticsTester {
+
+    StatisticsController statisticsController = new StatisticsController();
+
+    @Test
+    public void testGetDateFromString() {
+
+    }
 }
